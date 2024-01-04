47c8afdb97480b5cf3b64701db1f3136d538fe9d
Realmbird <happychristopher777@gmail.com>
2024-01-03 23:02:51 -0800

    loops through the array displays each example book

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+const myLibrary = ["bird", "are", "cool"];
 
 function Book() {
     //object constructor 
@@ -12,19 +12,19 @@ function addBookToLibrary() {
 function displayBooks() {
     //display books on site
     const title = document.querySelector('.title');
-    const table = document.createElement('h1');
+    const table = document.createElement('table');
     //wanted to see how to load element when page loads so did h1 
     //what I want 
     //const document.createElement('table');
-    table.textContent = "HI"
-    title.append(table);
+    
     // code to loop through books step 3
     //want books to display in table so I want to put everything into table and then load it into dom
     myLibrary.forEach(book => {
         const Lbook = document.createElement('td')
         Lbook.textContent = book
-        
+        table.append(Lbook)
     });
+    title.after(table);
 
-}
+    }
 displayBooks();
