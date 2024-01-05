b9b7514ce89addff9ca3cfdc27de771fc9b73069
Realmbird <happychristopher777@gmail.com>
2024-01-04 19:39:00 -0800

    Created event listeners that do not work for remvoe

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const myLibrary = [new Book("bird", "were the birds go", 100), new Book("blind", "scramblers", 200)]
+let myLibrary = [new Book("bird", "were the birds go", 100), new Book("blind", "scramblers", 200)]
 
 function Book(author, title, pages) {
     //object constructor 
@@ -6,6 +6,7 @@ function Book(author, title, pages) {
     this.author = author;
     this.title = title;
     this.pages = pages;
+   
 }
 
 function addBookToLibrary() {
@@ -27,6 +28,7 @@ function displayBooks() {
     
     // code to loop through books step 3
     //want books to display in table so I want to put everything into table and then load it into dom
+    let index = 0;
     myLibrary.forEach(book => {
         const BookRow = document.createElement('tr');
         BookRow.classList.add('new'); 
@@ -36,12 +38,17 @@ function displayBooks() {
             console.log(BookData)
             BookRow.append(BookData)
         }
+        const BDelete = document.createElement('td');
+        //BDelete.textContent = index;
+        BDelete.innerHTML = '<button class = "remove" value = "' + index + '">Remove Book</button>'
+        BookRow.append(BDelete);
         /*
         const BookAuthor = book.author;
         const BookTitle = book.title;
         const BookPages = book.pages;
         */
         table.append(BookRow)
+        index++;
     });
 
     }
@@ -49,7 +56,17 @@ displayBooks();
 const openbtn = document.querySelector("#new_book");
 const dialog = document.querySelector("dialog");
 const closebtn = document.querySelector(".close");
-const submitbtn = document.querySelector(".submit")
+const submitbtn = document.querySelector(".submit");
+const removebtns = document.querySelectorAll(".remove");
+removebtns.forEach(button => {
+    button.addEventListener("click", () => {
+        let i = parseInt(button.value);
+        //removes i
+        myLibrary = myLibrary.slice(0,i) + myLibrary.slice(i+1)
+        console.log()
+    })
+    displayBooks()
+})
 openbtn.addEventListener("click", () => {
     dialog.showModal()
 });
